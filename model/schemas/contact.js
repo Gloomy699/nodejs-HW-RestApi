edf0b70903ca50f6cb235307679cbfbc660bb89a
Yaroslav Vasylenko <gloomy.699@gmail.com>
2021-03-03 03:11:41 +0200

    1st commit 3HW

---
+++ model/schemas/contact.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set contact name"],
+      unique: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: [true, "Set contact email"],
+    },
+    phone: {
+      type: String,
+      required: [true, "Set contact phone"],
+    },
+    subscription: {
+      type: String,
+      default: "free",
+    },
+    password: {
+      type: String,
+      default: "password",
+    },
+    token: {
+      type: String,
+      default: "",
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+const Contact = model("contact", contactSchema);
+
+module.exports = Contact;
